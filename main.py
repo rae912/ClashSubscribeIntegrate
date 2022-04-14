ca716ecdfef32aff9f8625edfecc07878b169953
Cheng Wang <cheng.wang@shopee.com>
2022-04-14 10:39:53 +0800

    add new feature to append country flag before the server name

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import threading
 import json
 import logging
+from country_emoji_data import get_country_map
 
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
 config = {}
@@ -23,6 +24,8 @@ class Vmess(object):
         self.proxies_name = []
         self.config = ""
         self.global_proxies = {}
+        self.country_map = get_country_map()
+        self.global_proxies_with_country_flags = {}
 
     def parse_proxy(self, url):
         header = {
@@ -39,12 +42,12 @@ class Vmess(object):
     def collect_proxy(self):
         threads = [threading.Thread(target=self.parse_proxy, args=(url,)) for url in self.start_url]
         for t in threads:
-            logging.debug("Start thread: {}".format(t.native_id))
+            logging.debug("Start thread: {}".format(t.name))
             t.start()
 
         for t in threads:
             t.join(timeout=5)
-            logging.debug("End thread: {}".format(t.native_id))
+            logging.debug("End thread: {}".format(t.name))
 
     @staticmethod
     def import_config_index():
@@ -70,12 +73,17 @@ class Vmess(object):
         for p_name in sorted(self.global_proxies.keys()):
             p = self.global_proxies[p_name]
             self.proxies_name.append(p_name)
-            self.config += "  - {}\n".format(json.dumps(p, ensure_ascii=False))
+            country_flag = self.global_proxies_with_country_flags.get(p_name)
+            config = "  - {}\n".format(json.dumps(p, ensure_ascii=False))
+            # generate server config with contry flag
+            self.config += config.replace('{"name": "', '{"name": "' + country_flag)
 
         # write proxies name
         self.config += """\n\nproxy-groups:\n- name: AUTO\n  proxies:\n"""
         for n in self.proxies_name:
-            self.config += "  - \"{}\"\n".format(n)
+            country_flag = self.global_proxies_with_country_flags.get(n)
+            # generate server name with country flag
+            self.config += "  - \"{}{}\"\n".format(country_flag, n)
 
         # write end
         self.config += self.import_config_end()
@@ -108,10 +116,69 @@ class Vmess(object):
         response = requests.request("PATCH", url, headers=headers, data=payload)
         logging.debug(response.status_code)
 
+    # parse domain to ip with DOT DNS
+    def _domain_to_ip(self, domain_or_ip):
+        data = domain_or_ip.split(".")
+        data_type = "ip"
+        for i in data:
+            if not i.isdigit():
+                data_type = "domain"
+                break
+
+        if data_type == "domain":
+            url = "https://dns.google/resolve?name=" + domain_or_ip
+            r = requests.get(url)
+            resp = r.json().get("Answer")
+            if resp:
+                domain_or_ip = r.json().get("Answer")[0].get("data")
+            else:
+                domain_or_ip = "1.1.1.1"
+            
+            domain_or_ip = self._domain_to_ip(domain_or_ip)
+
+        logging.debug("Final IP: {}".format(domain_or_ip))
+        return domain_or_ip
+
+    # get country code by batch
+    def _iplist_to_country_code(self, ip_list):
+        url = "http://ip-api.com/batch"
+        ip_to_country_code = {}
+        r = requests.post(url, data=json.dumps(ip_list))
+        for i in r.json():
+            ip_to_country_code[i.get("query")] = i.get("countryCode")
+        
+        return ip_to_country_code
+
+    # query dns to get IP and append
+    def query_dns(self):
+        print(self.global_proxies)
+
+        domain_or_ip_list = list(map(lambda x: x.get("server"), self.global_proxies.values()))
+        ip_list_map = {}
+        for i in domain_or_ip_list:
+            ip_list_map[i] = self._domain_to_ip(i)
+        
+        ip_list = list(ip_list_map.values())
+        ip_to_country_code = self._iplist_to_country_code(ip_list)
+
+        for item in list(self.global_proxies.values()):
+            try:
+                domain = item["server"]
+                ip = ip_list_map[domain]
+                country_code = ip_to_country_code[ip]
+                self.global_proxies_with_country_flags[item["name"]] = self.country_map[country_code]
+            except KeyError:
+                logging.error("Key Error: {} {} {} {}".format(domain, ip, country_code, item["name"]))
+                self.global_proxies_with_country_flags[item["name"]] = self.country_map["US"]
+
+
+        print(self.global_proxies)
+
 
     def run(self):
         self.collect_proxy()
         self.clean_proxies()
+        self.query_dns()
         self.export_proxy()
         self.upload_to_gist()
 
